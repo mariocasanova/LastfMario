fd393e2d8a2868d43cf1e7a00777a3cad4442bd4
gs <gs@aula11-10.almata.es>
2019-04-30 15:30:53 +0200

    estructura afegida

--- js/constants.js
+++ js/constants.js
@@ -8,5 +8,6 @@ function myLoginFunction(){
 			params api_key ( my api key)
 			cb the web that goes when user is authenticated relative path ( depends on the server is launched): http://localhost:3000/mainpage.ht*/
 			var url= 'http://www.last.fm/api/auth/?api_key=d30c30f2e4eddeb7eac9ca3f90272243&cb=http://localhost:3000/mainpage.html';
-			window.location.replace(url);
+			window.location.replace(url);+
+		  document.onload();
 }
